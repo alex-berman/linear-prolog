182d214f9518dfc2abfbfc3664c6eef5038b02de
Alex Berman <alex@timebend.net>
2024-04-29 06:18:18 +0200

    Make rule language more compact

--- enthymemes.pl
+++ enthymemes.pl
@@ -1,17 +1,16 @@
 :- use_module(engine).
+:- ensure_loaded(operators).
 
 initial(start).
 initial(has_turn(user)).
 initial(moves([])).
 initial(qud([])).
 
-rule(start,
-     start -> heard(ask(question(user, road, R, pick(route, R, home))), user, system)).
+start :: (start -> heard(ask(question(user, road, R, pick(route, R, home))), user, system)).
 
-rule(hearAndRemember,
-     [heard(M, X, Y),
-      moves(Ms),
-      has_turn(X)] ->
-	 [moves([M|Ms]),
-	  pending(M),
-	  has_turn(Y)]).
+hearnAdRemember :: ([heard(M, X, Y),
+		     moves(Ms),
+		     has_turn(X)] ->
+			[moves([M|Ms]),
+			 pending(M),
+			 has_turn(Y)]).
