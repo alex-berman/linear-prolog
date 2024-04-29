4a8c794216bac2a0c80f8db342169eecdc78c3e6
Alex Berman <alex@timebend.net>
2024-04-29 06:05:32 +0200

    Create "engine" module

--- enthymemes.pl
+++ enthymemes.pl
@@ -1,4 +1,4 @@
-:- dynamic fact/2.
+:- use_module(engine).
 
 initial(start).
 initial(has_turn(user)).
@@ -15,51 +15,3 @@ rule(hearAndRemember,
 	 [moves([M|Ms]),
 	  pending(M),
 	  has_turn(Y)]).
-
-
-run :-
-    assert_initial_facts,
-    repeat,
-    print_state,
-    ( bagof(rule(RuleName, (Antecedent -> Consequent)),
-	  (rule(RuleName, (Antecedent -> Consequent)), antecedent_holds(Antecedent)),
-	  ApplicableRules) ->
-	  forall(member(rule(RuleName, (Antecedent -> Consequent)), ApplicableRules),
-		 (
-		     consume(Antecedent),
-		     establish(Consequent),
-		     write('Applied: '), write(RuleName), nl
-		 )),
-	  fail
-     ; true ).
-
-assert_initial_facts :-
-    forall(initial(Proposition), assert(fact(Proposition))).
-
-print_state :-
-    write('-------------------\nState:\n'),
-    forall(fact(Fact), (write('  '), write(Fact), nl)),
-    nl.
-
-antecedent_holds(Antecedent) :-
-    fact(Antecedent).
-antecedent_holds([Head|Tail]) :-
-    antecedent_holds(Head),
-    antecedent_holds(Tail).
-antecedent_holds([]).
-
-consume([]) :- !.
-consume([Head|Tail]) :-
-    !,
-    consume(Head),
-    consume(Tail).
-consume(Proposition) :-
-    retract(fact(Proposition)).
-
-establish([]) :- !.
-establish([Head|Tail]) :-
-    !,
-    establish(Head),
-    establish(Tail).
-establish(Proposition) :-
-    assert(fact(Proposition)).
