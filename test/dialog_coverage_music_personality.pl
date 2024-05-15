109cdda98720fd9e09806625184e76480db6584c
Alex Berman <alex@timebend.net>
2024-05-15 08:03:56 +0200

    M&P: Add unsupported test

--- test/dialog_coverage_music_personality.pl
+++ test/dialog_coverage_music_personality.pl
@@ -3,3 +3,15 @@ prediction_introverted{
 	turns: [utter(assert(not(extraverted)))]
     }.
 
+explanation_for_prediction{
+	facts: [
+	    not(extraverted),
+	    not(high_value(danceability_mean)),
+	    supports(not(high_value(danceability_mean)), not(extraverted))
+	],
+	turns: [
+	    utter(assert(not(extraverted))),
+	    heard(ask(why)),
+	    utter(assert(not(high_value(danceability_mean))))
+	]
+    }.
